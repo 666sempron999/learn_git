3e02a410c1f6162bd5906c7fe99f229eaa01b925
Your Name <boris.garbuzov@gmail.com>
2024-03-29 17:52:33 +0300

    chenge server port

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind(("", 1000))
+s.listen(1)
+
+connection, address = s.accept()
+
+print("message : {}".format(connection.recv(1024).decode()))
+print("from {}".format(address))
+
